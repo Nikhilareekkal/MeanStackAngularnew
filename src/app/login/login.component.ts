663e0e0122d615ff2847c55037113ddd4a1c6609
Nikhil <nikhilareekkal862@gmail.com>
2021-05-19 10:04:59 +0530

    data binding injection using ngsubmit form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../servics/data.service';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,10 @@ export class LoginComponent implements OnInit {
   aim="your perfect banking partner";
   accno="Acount number please";
   pswd="";
-  accountDetails= {
-    1000: { acno: 1000,  username: "userone", password: "userone", balance: 50000 },
-    1001: { acno: 1001,  username: "usertwo", password: "usertwo", balance: 5000 },
-    1002: { acno: 1002,  username: "userthree", password: "userthree", balance: 10000 },
-    1003: { acno: 1003,  username: "userfour", password: "userfour", balance: 6000 }
-} 
+  
 
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private dataService:DataService) { }
 
   ngOnInit(): void {
   }
@@ -36,7 +32,7 @@ export class LoginComponent implements OnInit {
     
     var acno=this.accno;
     var pswd=this.pswd;
-   let users=this.accountDetails;
+   let users=this.dataService.accountDetails;
     if (acno in users) {
         if (pswd == users[acno]["password"]) {
             alert("login successful")
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../servics/data.service';
 
 @Component({
   selector: 'app-register',
@@ -6,15 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  aim="register here";
-  accno="Acount number please";
+  
+  accno="";
   pswd="";
-uname="";
-  constructor() { }
+  uname="";
+  constructor(private dataService:DataService,private router:Router) { }
 
   ngOnInit(): void {
   }
-  register(){}
+  register(){
+    var uname=this.uname;
+    var acno=this.accno;
+    var pswd=this.pswd;
+
+    let user = this.dataService.accountDetails;
+
+    if(acno in user){
+      alert("user exist please login");
+  }
+  else{
+    user[acno]={
+    acno,
+    username:uname,
+    password:pswd,
+    balance:0}
+  }
+  alert("successfully registerd.......");
+  this.router.navigateByUrl("");
+  }
 
 
 }
