0ae773f008849af0d915a3a8523af183f8d8bbd0
Nikhil <nikhilareekkal862@gmail.com>
2021-05-19 23:27:04 +0530

    withdrwal and deposit in bank app

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../servics/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+dAccno="";
+dPswd="";
+dAmount="";
+wAccno="";
+wPswd="";
+wAmount="";
+  constructor(private dataService:DataService) { }
 
   ngOnInit(): void {
+  }
+  deposit(){
+    var accno=this.dAccno;
+    var pswd=this.dPswd;
+    var amount=this.dAmount;
+
+    const result = this.dataService.deposit(accno,pswd,amount)
+    if(result){
+      alert("the given amount "+amount+" has been credited... and the balance is:"+result);
+    }
+  }
+  withdrwal(){
+    var accno=this.wAccno;
+    var pswd=this.wPswd;
+    var amount=this.wAmount;
+
+    const result = this.dataService.withdrwal(accno,pswd,amount)
+    if(result){
+      alert("the given amount "+amount+"has been debited .....and the balance is:"+result)
+    }
+
+
+
   }
 
 }
--- src/app/servics/data.service.ts
+++ src/app/servics/data.service.ts
@@ -29,9 +29,9 @@ export class DataService {
   return true;
   }
  login(acno:any,pswd:any){
-  let users=this.accountDetails;
-  if (acno in users) {
-      if (pswd == users[acno]["password"]) {
+  let user=this.accountDetails;
+  if (acno in user) {
+      if (pswd == user[acno]["password"]) {
         return true;
                }
       else {
@@ -45,4 +45,49 @@ export class DataService {
   }
 
  }
+ deposit(accno:any,pswd:any,amt:any){
+   var amount = parseInt(amt);
+   let user = this.accountDetails;
+   if(accno in user){
+    if (pswd == user[accno]["password"]) {
+      user[accno]["balance"];
+      return true;
+   }
+   else{
+     alert("incorrect password");
+     return false;
+
+   }
+  }
+  else{
+    alert("invalid account");
+  return false;
+  }
+
+ }
+ withdrwal(accno:any,pswd:any,amt:any){
+   var amount = parseInt(amt);
+   let user = this.accountDetails;
+   if(accno in user){
+    if (pswd == user[accno]["password"]) {
+
+     if(user[accno]["balance"] > amount){
+       user[accno]["balance"]-=amount;
+       return user[accno]["balance"]
+     }
+     else{
+       alert("insufficient balance")
+       return false;
+     }
+   }
+   else{
+   alert("incorrect password")
+   return false;
+   }
+ }
+ else{
+   alert("invalid account")
+   return false;
+ }
+}
 }
